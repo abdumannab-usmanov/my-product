afde1fd6548b85c94a147919c462e088a65dec70
abdumannab-usmanov <usmanovabdumannab2307@gmail.com>
2022-06-06 00:54:39 +0500

    Add all things

---
+++ js/main.js
@@ -0,0 +1,17 @@
+$('.strategy__corusel').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    dots:true,
+    responsive: [
+        {
+            breakpoint: 521,
+            settings: "unslick"
+        }
+    ]
+  });
+    
+      
+
+  

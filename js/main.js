ca2e43a0be9eec3f909591c488cad90e08fbd894
abdumannab-usmanov <usmanovabdumannab2307@gmail.com>
2022-06-06 14:20:17 +0500

    Add some changes

--- js/main.js
+++ js/main.js
@@ -1,15 +1,9 @@
 $('.strategy__corusel').slick({
     slidesToShow: 3,
-    slidesToScroll: 1,
+    slidesToScroll: 3,
     autoplay: true,
     autoplaySpeed: 2000,
     dots:true,
-    responsive: [
-        {
-            breakpoint: 521,
-            settings: "unslick"
-        }
-    ]
   });
     
       
